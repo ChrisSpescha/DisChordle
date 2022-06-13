c15d28bd27350232d2ad721f44ffb1566ca57e79
ChrisSpescha <SpeschaChris@gmail.com>
2022-06-13 19:17:24 -0400

    Mania Ensues

--- app.py
+++ app.py
@@ -28,27 +28,59 @@ class Articles(db.Model):
 
 class ArticleSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'tile', 'body', 'date')
+        fields = ('id', 'title', 'body', 'date')
 
 
 article_schema = ArticleSchema()
 articles_schema = ArticleSchema(many=True)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/get', methods=['GET'])
 def get_articles():
-    return jsonify("Hello World")
+    all_articles = Articles.query.all()
+    results = articles_schema.dump(all_articles)
+    return jsonify(results)
+
+
+@app.route('/get/<id>/', methods=['GET'])
+def post_details(id):
+    article = Articles.query.get(id)
+    return article_schema.jsonify(article)
 
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_articles():
     title = request.json['title']
     body = request.json['body']
-
+    print(title)
+    print(body)
     articles = Articles(title, body)
     db.session.add(articles)
     db.session.commit()
     return article_schema.jsonify(articles)
+
+
+@app.route('/update/<id>/', methods=['PUT'])
+def update_article(id):
+    article = Articles.query.get(id)
+
+    title = request.json['title']
+    body = request.json['body']
+
+    article.title = title
+    article.body = body
+    db.session.commit()
+    return article_schema.jsonify(article)
+
+
+@app.route('/delete/<id>/', methods=['DELETE'])
+def article_delete(id):
+    print(request.method)
+    article = Articles.query.get(id)
+    db.session.delete(article)
+    db.session.commit()
+
+    return article_schema.jsonify(article)
 
 
 if __name__ == "__main__":
--- src + ui/app.js
+++ src + ui/app.js
@@ -3,20 +3,102 @@ const myform = document.getElementById('myform')
 const title = document.getElementById('title')
 const body = document.getElementById('description')
 
+const articles = document.getElementById('articles')
+
+let articleId;
+
 const insertData = (newData) => {
-    fetch('127.0.0.1:5000/add', {
+    fetch('http://127.0.0.1:5000/add', {
         method:'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-            body:JSON.stringify(newData)
+        body:JSON.stringify(newData)
         })
         .then(resp => resp.json())
         .then((data) => {
             console.log(data)
         })
-//        .catch(error => console.log(error))
+        .catch(error => console.log(error))
     }
+
+const getAllData = () => {
+
+    fetch('http://127.0.0.1:5000/get', {
+        method:'GET',
+        headers: {
+            'Content-Type': 'application/json'
+            }
+        })
+        .then(resp => resp.json())
+        .then(data => renderArticles(data))
+        .catch(error => console.log(error))
+
+    }
+
+const deleteData = (id) => {
+    fetch(`http://127.0.0.1:5000/delete/${id}/` , {
+        method:'DELETE',
+         headers:{
+            'Content-Type': 'application/json'
+        }
+    })
+    getAllData()
+}
+
+const getDataByID = (id) => {
+    fetch(`http://127.0.0.1:5000/get/${id}/`, {
+        method:'GET',
+         headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        renderOneItem(data)
+    })
+}
+
+const renderOneItem = (mydata) => {
+    title.value = mydata.title
+    body.value = mydata.body
+
+    articleId = mydata.id
+}
+
+const updateData = (articleId, mydata) => {
+    fetch(`http://127.0.0.1:5000/update/${articleId}/`, {
+        method:'PUT',
+         headers: {
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(mydata)
+    })
+    .then(resp => resp.json())
+    .then(() => {
+        getAllData()
+    })
+}
+
+
+
+function renderArticles(mydata){
+    articles.innerHTML = '';
+    mydata.forEach(data => {
+        articles.innerHTML += `
+        <div class = "card card-body my-2">
+            <p>${data.title}</p>
+            <p>${data.body}</p>
+            <h5>${data.date}</h5>
+
+            <p>
+            <button class = "btn btn-danger" onclick = "deleteData('${data.id}')">Delete</button>
+            <button class = "btn btn-success" onclick = "getDataByID('${data.id}')">Update</button>
+            </p>
+        </div>
+        `
+    })
+}
 
 myform.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -26,5 +108,12 @@ myform.addEventListener('submit', (e) => {
         body:body.value
     }
 
-    insertData(newData)
+    if(articleId) {
+        updateData(articleId, newData)
+    } else {
+        insertData(newData)
+    }
+    myform.reset()
 })
+
+getAllData()
